8a4a00235ba37f983e8cf5e76dbd83ba470b992a
mtcvz <meteceviz05@hotmail.com>
2020-07-21 23:39:35 +0300

    widgets moved to new folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,13 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'ui/my_app.dart';
 
 void main() {
-  runApp(MaterialApp(
-      theme:
-          ThemeData(primarySwatch: Colors.purple, accentColor: Colors.orange),
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "Flutter Examples",
-              style: TextStyle(fontSize: 24.0, color: Colors.white),
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              debugPrint("Tiklandi");
-            },
-            child: Icon(Icons.add, color: Colors.white),
-          ),
-          body: Column(
-            children: <Widget>[
-              Text(
-                "Resim ve Buton Turleri",
-                style: TextStyle(fontSize: 24.0),
-              ),
-              IntrinsicHeight(
-                child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                    color: Colors.red.shade200,
-                    child: Column(
-                      children: <Widget>[
-                        Image.asset("assets/images/example.png"),
-                        Text("Asset Image"),
-                      ],
-                    ),
-                  ),),
-                  Expanded(
-                    child:Container(
-                    color: Colors.red.shade200,
-                    child: Column(
-                      children: <Widget>[
-                        //Image.network("https://mediatrend.mediamarkt.com.tr/wp-content/uploads/2017/02/2017_subat_03.jpg"),
-                        FadeInImage.assetNetwork(placeholder: "assets/images/example.png",  // fadeinImage foto yuklenene kadar gecici fotoyu tutar.
-                            image: "https://mediatrend.mediamarkt.com.tr/wp-content/uploads/2017/02/2017_subat_03.jpg"),
-                        Text("Network Image"),
-                      ],
-                    ),
-                  ),),
-                  Expanded(
-                    child:Container(
-                    color: Colors.red.shade200,
-                    child: Column(
-                      children: <Widget>[
-                        CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage("assets/images/example.png"),
-                        ),
-                        Text("Circle Avatar"),
-                      ],
-                    ),
-                  ),
-                  ),
-                ],
-               ),
-              ),
-            ],
-          )))); //runapp
+  var myApp = new MyApp();
+  runApp(myApp); //runapp
 }
+
 
 /*void main(){
   runApp(new MaterialApp(
